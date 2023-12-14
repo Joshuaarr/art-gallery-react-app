03dc25b889bff31aacdda4c8a6ca7c27359a7959
Joshua <raoyunxuan@outlook.com>
2023-12-14 00:49:13 -0800

    trying to deploy

--- src/project/Users/follows/client.js
+++ src/project/Users/follows/client.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const API_BASE = process.env.REACT_APP_API_BASE;
 const client = axios.create({
   withCredentials: true,
-  baseURL: `${API_BASE}/api/users`,
+  baseURL: `${API_BASE}/users`,
 });
 
 export const userFollowsUser = async (followed) => {
